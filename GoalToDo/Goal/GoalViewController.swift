2a22af806fad53fae3e84eaae0ef45d7dc778783
Sena <senana0116@icloud.com>
2022-04-16 12:05:25 +0900

    var sena = 1 の追加

--- GoalToDo/Goal/GoalViewController.swift
+++ GoalToDo/Goal/GoalViewController.swift
@@ -11,6 +11,7 @@ class CountDownTimerViewController: UIViewController {
     
     var dyingDate: Date?
     var count = 0
+    var sena = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
